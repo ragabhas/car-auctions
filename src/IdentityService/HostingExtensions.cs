dda8851d60c433d07393f80baa0b731cca9bcc54
Hassan Ragab <hassan.ragab@thermofisher.com>
2024-11-29 14:19:28 +0100

    add postgres migration

--- src/IdentityService/HostingExtensions.cs
+++ src/IdentityService/HostingExtensions.cs
@@ -36,6 +36,12 @@ internal static class HostingExtensions
             .AddInMemoryClients(Config.Clients)
             .AddAspNetIdentity<ApplicationUser>();
 
+        builder.Services.ConfigureApplicationCookie(options =>
+        {
+            options.Cookie.SameSite = SameSiteMode.Lax;
+
+        });
+
         builder.Services.AddAuthentication();
 
         return builder.Build();
