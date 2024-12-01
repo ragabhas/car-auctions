cb96bfd3d73772775b3a612375284f2c31efd8c6
Hassan Ragab <ragab.hassan@outlook.com>
2024-12-01 21:04:24 +0100

    dockerize search service

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -15,6 +15,12 @@ builder.Services.AddMassTransit(x =>
   x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false));
   x.UsingRabbitMq((context, cfg) =>
   {
+    cfg.Host(builder.Configuration["RabbitMq:Host"], "/", h =>
+        {
+          h.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+          h.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
+
     cfg.ReceiveEndpoint("seatch-auction-created", e =>
     {
       e.UseMessageRetry(r => r.Interval(5, 1000));
